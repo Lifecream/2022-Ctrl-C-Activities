813b6b11de7536a2257ccd4c46b7c3cfded7dd9c
Lifecream <101922012+Lifecream@users.noreply.github.com>
2022-04-06 15:54:08 +0900

    Create 5. A+B.c

---
+++ 22.04.06/5. A+B.c
@@ -0,0 +1,19 @@
+/*
+백준 1000,1001,10998번 
+https://www.acmicpc.net/problem/1000
+https://www.acmicpc.net/problem/1001
+https://www.acmicpc.net/problem/10998
+*/
+
+//입력받은 두 정수로 나누기를 제외한 사칙연산에 대해 학습
+
+#include <stdio.h>
+
+int main(void){
+        int a,b;
+        scanf("%d %d",&a, &b);
+        printf("%d %d",a+b);
+        //printf("%d %d",a-b);
+        //printf("%d %d",a*b);
+        return 0;
+}

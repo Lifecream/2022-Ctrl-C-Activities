337d0171b7f0ee16d2786f89139b31d25d1b99b7
Lifecream <101922012+Lifecream@users.noreply.github.com>
2022-04-06 15:49:26 +0900

    Create 4. Newline '\n'.c

---
+++ 22.04.06/4. Newline '\n'.c
@@ -0,0 +1,12 @@
+/*
+백준 10718번
+https://www.acmicpc.net/problem/10718
+*/
+
+//줄 바꿈에 사용되는 개행문자 '\n'에 대해 학습
+
+int main(){
+        printf("강한친구 대한육군\n");
+        printf("강한친구 대한육군");
+        return 0; 
+}

6b8319826c4976191fb4992de695e8d251c3fcde
Lifecream <101922012+Lifecream@users.noreply.github.com>
2022-05-11 13:52:39 +0900

    Create 2.c

---
+++ 22.04.20/2.c
@@ -0,0 +1,20 @@
+/*
+백준 10869번
+https://www.acmicpc.net/problem/10869
+*/
+
+//22.04.06 복습
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int a,b;
+	scanf("%d %d",&a, &b);
+	printf("%d\n", a+b);
+	printf("%d\n", a-b);
+	printf("%d\n", a*b);
+	printf("%d\n", a/b);
+	printf("%d", a%b);
+	return 0;
+}

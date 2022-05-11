bd2666226d737b79a8422ce24f07b3e5ddba8fb9
Lifecream <101922012+Lifecream@users.noreply.github.com>
2022-05-11 14:04:05 +0900

    Create 1. Year Caculate.c

---
+++ 1. Year Caculate.c
@@ -0,0 +1,15 @@
+/*
+백준 18108번
+https://www.acmicpc.net/problem/18108
+*/
+
+//input, output review
+
+#include <stdio.h>
+
+int main(void) {
+	int a;
+	scanf("%d",&a);
+	printf("%d",a-543);
+	return 0;
+}

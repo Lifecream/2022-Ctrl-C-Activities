b6b8448880488563ce5192745eed0afdc8b3ff06
Lifecream <101922012+Lifecream@users.noreply.github.com>
2022-05-11 13:51:03 +0900

    Create 1. Real Num Caculate.c

---
+++ 1. Real Num Caculate.c
@@ -0,0 +1,16 @@
+/*
+백준 10869번
+https://www.acmicpc.net/problem/10869
+*/
+
+//실수 자료형인 double과 %lf를 활용한 실수 입출력 및 소수점 한정시키기
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	double a,b;
+	scanf("%lf %lf",&a, &b);
+	printf("%.9lf", a/b);
+	return 0;
+}

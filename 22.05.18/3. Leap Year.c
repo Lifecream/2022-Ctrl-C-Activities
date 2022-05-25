6a1b8ada059a70ba9cd3017923499980ab68e2d6
Lifecream <lifecream@icloud.com>
2022-05-25 11:06:52 +0900

    Create 3. Leap Year.c

---
+++ 22.05.18/3. Leap Year.c
@@ -0,0 +1,14 @@
+/*
+백준 2753번
+https://www.acmicpc.net/problem/2753
+*/
+
+#include <stdio.h>
+
+int main(void) {
+	int n;
+	scanf("%d",&n);
+	if(n % 4 == 0 && n % 100 != 0 || n % 400 == 0) printf("1");
+	else printf("0");
+	return 0;
+}

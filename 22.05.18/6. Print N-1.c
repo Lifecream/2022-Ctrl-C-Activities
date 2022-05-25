498b1a6832604c218e89dd237eb44d9706cadbf5
Lifecream <lifecream@icloud.com>
2022-05-25 11:14:09 +0900

    Create 6. Print N-1.c

---
+++ 22.05.18/6. Print N-1.c
@@ -0,0 +1,15 @@
+/*
+백준 2742번
+https://www.acmicpc.net/problem/2742
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int a;
+	scanf("%d",&a);
+	for(int i = a; i>=1; i--){
+		printf("%d\n", i);
+	}
+}
